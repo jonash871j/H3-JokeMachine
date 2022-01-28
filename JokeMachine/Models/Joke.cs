78125e0ba4242e63927c168d68e3644ea3c1b0d5
Jonas Hansen <jonash871j@gmail.com>
2022-01-28 10:56:12 +0100

    Minor change

--- JokeMachine/Models/Joke.cs
+++ JokeMachine/Models/Joke.cs
@@ -6,10 +6,8 @@ namespace JokeMachine.Models
     {
         public string? Question { get; set; }
         public string? Answer { get; set; }
-
         [JsonIgnore]
         public JokeCategorie Categorie { get; set; }
-
         [JsonIgnore]
         public string? Language { get; set; }
     }
